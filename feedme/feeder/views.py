4c1ed74d7c2538fd2cf776565395ebf7d9c6b909
Christopher Dieckhaus <cpd18@duke.edu>
2015-10-15 23:35:51 +0000

    Front end updates

--- feedme/feeder/views.py
+++ feedme/feeder/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Restaurant
 
 def index(request):
     context = {}
     return render(request, 'index.html', context)
 
 def result(request):
-    context = {}
+    restaurant = Restaurant.objects.all()[5]
+    context = {'restaurant': restaurant}
     return render(request, 'result.html', context)
