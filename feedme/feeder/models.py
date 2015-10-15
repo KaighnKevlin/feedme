77411027e18cb003dd0e985c82bbbdc33eceb5ee
Christopher Dieckhaus <cpd18@duke.edu>
2015-10-15 17:03:21 +0000

    Add results template

--- feedme/feeder/models.py
+++ feedme/feeder/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=15)
+    zip_code = models.IntegerField()
+    rating = models.IntegerField()
--- feedme/feeder/views.py
+++ feedme/feeder/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 def index(request):
     context = {}
     return render(request, 'index.html', context)
+
+def result(request):
+    context = {}
+    return render(request, 'result.html', context)
