de4c44bc8ef18eaac89c1c729b05aa789e796118
Christopher Dieckhaus <cpd18@duke.edu>
2015-10-15 18:21:10 +0000

    Script for importing data to db

--- feedme/feeder/import_data.py
+++ feedme/feeder/import_data.py
@@ -5,7 +5,26 @@ print "starting data import"
 
 #execfile('feeder/import_data.py')
 
-with open('feeder/yelp_data.json') as data_file:    
-    print data_file
+with open('feeder/yelp_data_durham.json') as data_file:    
     data = json.load(data_file)
-    print data
+    for rst in data:
+      r = Restaurant(name=rst["name"], address=rst["address"], city=rst["city"], zip_code=rst["zip"], rating=rst["rating"])
+      r.save()
+
+with open('feeder/yelp_data_ch.json') as data_file:    
+    data = json.load(data_file)
+    for rst in data:
+      r = Restaurant(name=rst["name"], address=rst["address"], city=rst["city"], zip_code=rst["zip"], rating=rst["rating"])
+      r.save()
+      
+with open('feeder/yelp_data_dc.json') as data_file:    
+    data = json.load(data_file)
+    for rst in data:
+      r = Restaurant(name=rst["name"], address=rst["address"], city=rst["city"], zip_code=rst["zip"], rating=rst["rating"])
+      r.save()
+      
+with open('feeder/yelp_data_nyc.json') as data_file:    
+    data = json.load(data_file)
+    for rst in data:
+      r = Restaurant(name=rst["name"], address=rst["address"], city=rst["city"], zip_code=rst["zip"], rating=rst["rating"])
+      r.save()
